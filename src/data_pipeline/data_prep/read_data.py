9405f87a2505e64eb99de9e721b579b417135608
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-10-20 11:11:43 -0600

    Reading data from jsonl file.

---
+++ src/data_pipeline/data_prep/read_data.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+import time
+
+spark = SparkSession \
+    .builder \
+    .appName("SemanticSearch") \
+    .getOrCreate()
+
+start_time = time.time()
+
+reddit_df = spark.read.option("multiLine" , "true").json("data/RC_2022-06")
+
+reddit_df.printSchema()
+
+print("Process took : {} seconds".format(time.time() - start_time))
+
+
