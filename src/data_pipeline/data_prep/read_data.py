4e8afbeec5fdae234bcd979b2e55e39c29193d89
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-10-20 11:59:59 -0600

    more debugging info for dropping_duplicates

--- src/data_pipeline/data_prep/read_data.py
+++ src/data_pipeline/data_prep/read_data.py
@@ -13,9 +13,13 @@ reddit_df = spark.read.option("multiLine" , "true").json("data/RC_2022-06")
 
 reddit_df.printSchema()
 
+print(reddit_df.count())
+
 reddit_df = reddit_df.dropDuplicates(['id'])
 
 print(reddit_df.head(10))
+
+print(reddit_df.count())
 
 print("Process took : {} seconds".format(time.time() - start_time))
 
