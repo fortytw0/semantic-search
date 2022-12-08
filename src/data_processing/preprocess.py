0c2c709f17db2c13334f125fd712cf3b8469e170
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-12-07 17:29:16 -0700

    added spark module

---
+++ src/data_processing/preprocess.py
@@ -0,0 +1,20 @@
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+data_files = sys.argv[1]
+
+print('Received Data File : ' , data_files)
+
+spark = SparkSession.builder.getOrCreate()
+
+reddit_df = spark.read.json(data_files)
+
+reddit_df = reddit_df.withColumn('body_lowercase' , F.lower(F.col("body")))
+
+reddit_df = reddit_df.rdd.zipWithIndex().toDF()
+
+reddit_df=reddit_df.select(F.col("_1.*") , F.col("_2").alias('index'))
+
+reddit_df.write.json(data_files+'_PROCESSED')
