ab0e164bd184c66fadb75a62af15cb993c4c5e66
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-12-14 18:30:43 -0700

    Final commit for search client

--- src/client/search_client.py
+++ src/client/search_client.py
@@ -66,6 +66,12 @@ def get_finished_results(token) :
     endpoint = 'search/reddit/v1/finished/'
     return requests.get(parse.urljoin(URL , endpoint) , headers={'Authorization' : 'Token {}'.format(token)})
 
+def print_elipsis(s) : 
+
+    for i in range(s) :
+        print('...')
+        time.sleep(1) 
+
 
 print('Hello! This is a demo for our Reddit Semantic Search framework!')
 
@@ -73,7 +79,7 @@ username = 'mycoolusername'
 password = 'mycoolpassword1234'
 email = 'mycoolemail@123.com'
 
-time.sleep(3)
+print_elipsis(3)
 
 print('We begin by creating an account...')
 
@@ -84,10 +90,10 @@ print('Email : ' , email)
 
 response = create_account(username, password, email)
 
-print('Json Response after account creation request: ' , response.json())
+print('Reply from API endpoint: ' , response.json())
 print('We have been successfully able to create an account...')
 
-time.sleep(3)
+print_elipsis(3)
 
 print('Now we are going to login using our new credentials.')
 
@@ -97,16 +103,21 @@ token = response.json()['auth_token']
 
 print('We have received the token {} as a response of our login request. Please keep it secret.'.format(token))
 
-time.sleep(3)
+print_elipsis(3)
 
-print('Now we are going to send a search query to the server...')
+print('Now we are going to send a search query to the server...\n')
+print('The details of our request are given below : \n')
 
-print('The details of our request are given below : ')
+print_elipsis(3)
 
 print("search_string = 'I think Messi is way better than Ronaldo'")
 print("subreddits = 'soccer'")
 print("filter_keywords = ''")
 print("description = 'I have an incredibly parochial view on soccer'")
+
+print_elipsis(3)
+
+print('Sending the actual request...')
 
 search_string = 'I think Messi is way better than Ronaldo'
 subreddits = 'soccer'
@@ -115,14 +126,50 @@ description = 'I have an incredibly parochial view on soccer'
 
 response = reddit_search(token , search_string, subreddits , filter_keywords , description)
 
-print('The response to our search request is : ')
+job_name = response.json()['job_name']
+
+print('We have been assigned a job name :  ' , job_name)
 print(response.json())
 
-time.sleep(3)
-print('Listing all my jobs : ')
+print_elipsis(3)
+print('Let us take a look at all the jobs that I have submitted : ')
 response = list_my_jobs(token)
 
-print('Response to our request is : ')
 print(response.json())
+
+print_elipsis(3)
+print('Let us monitor the status of my job : ')
+
+job_finished = False 
+
+while not job_finished : 
+
+    response = get_job_status(token , job_name)
+    status = response.json()['job_name']
+    if status != 'finished' : 
+        print('Current status : ' , status)
+        print_elipsis(1)
+    else : 
+        print('The job has finished!')
+        job_finished = True
+
+print('Let us look at the results : ')
+
+response = get_finished_results(token)
+
+for result in response.json() : 
+
+    if len(result) < 10 : 
+        for r in result :
+            print(r)
+            print('\n') 
+
+    else : 
+        for r in result[:10] : 
+            print(r)
+            print('\n')
+
+
+
 
 
